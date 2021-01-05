abc60acd77b2ba50d67a512b1c7205f6d7fe8528
Bekzod <76199273+jetski1@users.noreply.github.com>
2021-01-05 16:00:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let Numb1 = +prompt('Введите число')
 Numb1 = !isNaN(Numb1) && Numb1 > 0 ? Numb1 : alert('Вы ввели 0 или не число, попробуйте еще раз и введите число больше 0')
 let Numb2 = +prompt('Введите степень')
-Numb2 = !isNaN(Numb2) && Numb2 > 0 ? Numb2 : alert('2')
+Numb2 = !isNaN(Numb2) && Numb2 == 0 ? Numb2 : 2
  let answer = Numb1
 
 for(  i=1; i<Numb2; i++    ){
